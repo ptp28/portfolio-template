28dfedb0b5dfbfce859603df41096781cae302ad
ptp28 <parthparikh28@gmail.com>
2024-10-22 21:34:48 -0400

    Re-added lorem ipsum.

--- src/ContentPane/Pages/AboutPage/AboutPage.tsx
+++ src/ContentPane/Pages/AboutPage/AboutPage.tsx
@@ -12,20 +12,19 @@ export default function AboutPage(props) {
 
             <NameComponent/>
 
-            <Typography
-                sx={{marginTop: 7, fontFamily: 'Mulish', fontWeight: '500', fontSize: '20px'}}
-                component="p">
-                Student at Northeastern University, Boston, pursuing a graduate degree in Computer Science.
-                <br/>
-                SDE-intern at SOLIDWORKS, Dassault Systèmes.
-                <br/>
-                Previously worked at e-Yantra, IIT-Bombay as a Sr. Project Technical Researcher.
-                <br/>
-                Graduated from Pune University with a Bachelor's degree in Computer Engineering.
-                <br/>
-                Always interested to learn more about computer science, technology, project ideas, etc.
-                <br/>
-                Enjoy playing tennis and sailing.
+            <Typography sx={{marginTop: 7, fontSize: 20, fontWeight: "500"}}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non
+                enim praesent elementum facilisis leo vel. Risus at ultrices mi tempus
+                imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus.
+                Convallis convallis tellus id interdum velit laoreet id donec ultrices.
+                Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
+                adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra
+                nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum
+                leo. Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt lobortis
+                feugiat vivamus at augue. At augue eget arcu dictum varius duis at
+                consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa
+                sapien faucibus et molestie ac.
             </Typography>
 
             <ContactButtons/>
